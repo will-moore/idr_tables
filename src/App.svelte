7d0acb80e9771a2f2bfd36926957d45f6f89e30a
William Moore <w.moore@dundee.ac.uk>
2022-04-03 13:42:12 +0100

    Split screen to give space for tables

--- src/App.svelte
+++ src/App.svelte
@@ -3,26 +3,49 @@
     import TreeChildren from './TreeChildren.svelte';
 </script>
 
-<Nav></Nav>
 <main>
+    <nav>
+        <Nav></Nav>
+    </nav>
+    <section class="treeSection">
+        <h1>IDR tables</h1>
+        <TreeChildren></TreeChildren>
+    </section>
+    <section>
 
-	<h1>IDR tables</h1>
-	
-    <TreeChildren></TreeChildren>
+    </section>
 </main>
 
 <style>
 	main {
-		text-align: center;
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+	}
+
+    nav {
+        height: 48px;
+        margin: 0;
+        padding: 0;
+    }
+
+    section {
+        flex: 1;
+        overflow: auto;
+        width: 100%;
+    }
+
+    .treeSection {
+        text-align: center;
 		padding: 1em;
 		margin: 0 auto;
-	}
+        border-bottom: 1px solid #ddd;
+    }
 
 	h1 {
 		text-transform: uppercase;
 		font-size: 3em;
 		font-weight: 100;
-        margin-top: 40px;
 	}
 
 	/* @media (min-width: 640px) {
